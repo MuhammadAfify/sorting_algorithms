e4adb649108474fdabe4d27898bc2f04fe8cf495
MuhammadAfify <muhammadafefy@gmail.com>
2024-02-20 14:15:08 -0800

    sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * _swap - Swap two integers
+ *
+ * @x: The first integer to swap.
+ * @y: The second integer to swap.
+ */
+void _swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order
+ *
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubble = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubble == false)
+	{
+		bubble = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubble = false;
+			}
+		}
+		len--;
+	}
+}

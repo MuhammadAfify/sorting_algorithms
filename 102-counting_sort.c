cf7daa20ae1408a6f906641482aa6d522b1ac4dc
MuhammadAfify <muhammadafefy@gmail.com>
2024-02-24 13:47:25 -0800

    sorting algorthims

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,7 +2,7 @@
 
 /**
  * get_max - Get the maximum value in an array of integers
- * 
+ *
  * @array: An array of integers
  * @size: The size of the array
  * Return: The maximum integer in the array
